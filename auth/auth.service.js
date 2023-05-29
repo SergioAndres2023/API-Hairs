1c3dac3c48dc66bf16b03a3025d72bbaefa9b445
TuWebLogin <tuweblogin@gmail.com>
2023-05-29 19:41:56 +0200

    auth

---
+++ auth/auth.service.js
@@ -0,0 +1,75 @@
+import jwt from 'jsonwebtoken';
+import { hashSync, compareSync } from 'bcrypt';
+// import * as usersRepository from '../users/users.repository.js';
+
+function getToken({ username }) {
+  const payload = {
+    username,
+  };
+
+  const token = jwt.sign(payload, 'secretWord', {
+    expiresIn: 60 * 60,
+  });
+
+  return token;
+}
+
+export async function register({ username, password }) {
+  const hashedPassword = hashSync(password, 10);
+  const dbUser = await usersRepository.create({ username, password: hashedPassword });
+  if (!dbUser) {
+    const myError = {
+      status: 500,
+      message: 'Some problem creating the user',
+    };
+
+    throw new Error(JSON.stringify(myError));
+  }
+
+  const token = getToken({ username: dbUser.username });
+  if (!token) {
+    const myError = {
+      status: 500,
+      message: 'Some problem generating token',
+    };
+
+    throw new Error(JSON.stringify(myError));
+  }
+
+  return token;
+}
+
+export async function login({ username, password }) {
+  const dbUser = await usersRepository.getByUsername({ username });
+  if (!dbUser) {
+    const myError = {
+      status: 401,
+      message: 'Wrong credentials',
+    };
+
+    throw new Error(JSON.stringify(myError));
+  }
+
+  // const isSamepassword = dbUser.password === password;
+  const isSamePassword = compareSync(password, dbUser.password);
+  if (!isSamePassword) {
+    const myError = {
+      status: 401,
+      message: 'Wrong credentials',
+    };
+
+    throw new Error(JSON.stringify(myError));
+  }
+
+  const token = getToken({ username: dbUser.username });
+  if (!token) {
+    const myError = {
+      status: 500,
+      message: 'Some problem generating token',
+    };
+
+    throw new Error(JSON.stringify(myError));
+  }
+
+  return token;
+}
