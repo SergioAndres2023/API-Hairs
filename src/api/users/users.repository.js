b96be62223a850359524aeacb1080cf51eae971f
TuWebLogin <tuweblogin@gmail.com>
2023-06-02 19:25:38 +0200

    login

--- src/api/users/users.repository.js
+++ src/api/users/users.repository.js
@@ -5,8 +5,3 @@ export async function create({ username, password }) {
   return newUser;
 }
 
-export async function remove({ id }) {
-  const users = await userModel.findByIdAndDelete(id);
-  console.log('delete', users);
-  return {};
-}
