738fbe64a61179714b3589aacbdf854b7c4dcafb
Your NameCarlos <you@example.com>
2023-06-02 19:04:08 +0200

    vamos que nos vamos

--- src/api/services/services.controller.js
+++ src/api/services/services.controller.js
@@ -1,5 +1,10 @@
 import * as servicesService from './services.service.js';
 
+export async function getAll(req, res) {
+  const services = await servicesService.getAll();
+  res.json(services);
+}
+
 export async function create(req, res) {
   const serviceData = req.body;
   const services = await servicesService.create({ serviceData });
--- src/api/services/services.router.js
+++ src/api/services/services.router.js
@@ -3,6 +3,8 @@ import * as servicesController from './services.controller.js';
 
 const router = Router();
 
+router.get('/all', servicesController.getAll);
+
 router.post('/', servicesController.create);
 
 export default router;
