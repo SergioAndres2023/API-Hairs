cd58b37541fefc037053104e17547ab035ecb6a2
Carlos M Odero <jcm.odero@gmail.com>
2023-06-13 23:01:53 +0200

    usersGetAllConfirmed

--- src/api/users/users.repository.js
+++ src/api/users/users.repository.js
@@ -2,7 +2,10 @@ import userModel from './users.model.js';
 
 export async function getAll() {
   const users = await userModel
-    .find({ deleted: false })
+    .find({
+      deleted: false,
+      confirmed: true,
+    })
     .lean();
 
   return users;
